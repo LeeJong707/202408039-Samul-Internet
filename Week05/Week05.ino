91c45db392ceda4098742ece8a13ed9fc332a259
LeeJong707 <ljwomlj0403@gmail.com>
2025-04-15 16:18:23 +0900

    code

---
+++ Week05/Week05.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  char nm[7] = "gildong";
+  Serial.println(nm);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
